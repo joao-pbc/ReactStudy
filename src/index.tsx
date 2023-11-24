5381cbfeeadbb1e60b01cea905dc89b440c4062b
João Pedro Bove da Costa <joao.bove@hotmail.com>
2023-11-24 09:25:08 -0300

    InitialComit
    ReactStudy

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+function Greeting(){
+  return <h1>Hello ppl</h1>;
+}
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'));
