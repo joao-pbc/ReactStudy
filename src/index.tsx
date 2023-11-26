5aa09aae46258cc9bc46fa4a5ccadf2b3528af1f
João Pedro Bove da Costa <joao.bove@hotmail.com>
2023-11-26 10:59:07 -0300

    BookList
    Redirecionado o projeto para criar componentes de um livro e exibir eles em um grid

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import './index.css';
 
-function Greeting(){
-  return <h1>Hello ppl</h1>;
+function BestSellingBooks() {
+  return (
+    <>
+      <section className='bookList'>
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+      </section>
+    </>
+  )
 }
 
-ReactDOM.render(<Greeting/>, document.getElementById('root'));
+const Book = () => {
+
+  return(
+    <>
+      <article className='books'>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg" alt="livro" />
+        <h1>Book name</h1>
+        <p>price</p>
+        <p>Description</p>
+      </article>
+    </>
+  );
+}
+
+/*const BookShelf = () => {
+
+  return (
+    <>
+    </>
+  );
+
+}*/
+
+ReactDOM.render(<BestSellingBooks />, document.getElementById('root'));
