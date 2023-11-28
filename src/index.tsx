a2cfda9f382d5933d5cc24e298e80da61298b0d3
João Pedro Bove da Costa <joao.bove@hotmail.com>
2023-11-28 10:32:04 -0300

    Componentes e CSS
    Acrescentado levemente CSS e alguns componentes

--- src/index.tsx
+++ src/index.tsx
@@ -2,44 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const bookProps = {
+  image: "https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+  title: "Book name",
+  price: "price"
+}
+
+
 function BestSellingBooks() {
   return (
     <>
       <section className='bookList'>
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book 
+          image = {bookProps.image} 
+          title ={bookProps.title}
+          price = {bookProps.price}
+        />
       </section>
     </>
   )
 }
 
-const Book = () => {
+const Book = ({image, title, price}) => {
 
   return(
     <>
       <article className='books'>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg" alt="livro" />
-        <h1>Book name</h1>
-        <p>price</p>
-        <p>Description</p>
+        <img src={image} alt="livro" />
+        <h1>{title}</h1>
+        <p>{price}</p>
       </article>
     </>
   );
 }
-
-/*const BookShelf = () => {
-
-  return (
-    <>
-    </>
-  );
-
-}*/
 
 ReactDOM.render(<BestSellingBooks />, document.getElementById('root'));
