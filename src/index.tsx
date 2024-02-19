93faf7a453c1fac61000fad4c9e741cfb852bed9
João Pedro Bove da Costa <88252025+joao-pbc@users.noreply.github.com>
2024-02-18 21:33:44 -0300

    Iterating array of props
    Used a array of props to iterate each book; Used spread operators to send the objects into the Book

--- src/index.tsx
+++ src/index.tsx
@@ -2,37 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const bookProps = {
+const bookProps = [{
   image: "https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
   title: "Book name",
   price: "price"
-}
+},
+{
+  image: "https://images-na.ssl-images-amazon.com/images/I/51ha90+JbfL._AC_UL600_SR600,400_.jpg",
+  title: "He leadeth me",
+  price: "US$ 12,21"
+}]
 
 
 function BestSellingBooks() {
   return (
     <>
       <section className='bookList'>
-        <Book 
-          image = {bookProps.image} 
-          title ={bookProps.title}
-          price = {bookProps.price}
-        />
+        {bookProps.map((book) => {
+          return (
+           <Book {...book}/>
+          );
+        })}
       </section>
     </>
   )
 }
 
-const Book = ({image, title, price}) => {
-
-  return(
-    <>
-      <article className='books'>
-        <img src={image} alt="livro" />
-        <h1>{title}</h1>
-        <p>{price}</p>
-      </article>
-    </>
+const Book = ({ image, title, price }) => {
+  return (
+    <article className='books'>
+      <img src={image} alt="livro" />
+      <h1>{title}</h1>
+      <p>{price}</p>
+    </article>
   );
 }
 
