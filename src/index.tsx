e233c9e899fa16c8bc225322f61fb564ae9be2b8
João Pedro Bove da Costa <88252025+joao-pbc@users.noreply.github.com>
2024-02-18 22:14:50 -0300

    Testing EVENTS
    Added a click event that prints "Click"

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,11 @@ const bookProps = [{
   image: "https://images-na.ssl-images-amazon.com/images/I/51ha90+JbfL._AC_UL600_SR600,400_.jpg",
   title: "He leadeth me",
   price: "US$ 12,21"
+},
+{
+  image: "https://images-na.ssl-images-amazon.com/images/I/913C+MR3S5L._AC_UL600_SR600,400_.jpg",
+  title: "The Woman",
+  price: "US$ 19,21"
 }]
 
 
@@ -29,11 +34,18 @@ function BestSellingBooks() {
 }
 
 const Book = ({ image, title, price }) => {
+ 
+  const pimba = () => {
+    alert("click");
+  }
   return (
     <article className='books'>
       <img src={image} alt="livro" />
       <h1>{title}</h1>
       <p>{price}</p>
+      <button type="button" onClick={pimba}>
+        Event test  
+      </button>
     </article>
   );
 }
